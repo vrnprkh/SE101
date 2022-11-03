8f312ff66c0d7b56b3a4dfcc655975c4c98c2fea
em-w <emilywang4321@gmail.com>
2022-11-03 09:56:54 -0400

    blink test code

--- hardware interfacing/main.py
+++ hardware interfacing/main.py
@@ -1,10 +1,12 @@
-import pyfirmata
+from pyfirmata import util, Arduino
 import time
 
-board = pyfirmata.Arduino('/dev/ttyACM0')
-
+board = Arduino('COM5')
 while True:
-    board.digital[13].write(1)
-    time.sleep(1)
-    board.digital[13].write(0)
-    time.sleep(1)
+    board.iterate()
+    input7 = board.get_pin('d:7:i')
+    while(input7.read() == 1):
+        board.digital[13].write(1)
+        time.sleep(1)
+        board.digital[13].write(0)
+        time.sleep(1)
