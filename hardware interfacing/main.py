8aad143c5219df850203720a61aa7b522708d546
nandanjp <nandan.jp17@gmail.com>
2022-11-03 09:59:45 -0400

    Added Code from Theron' Wierenga's Project that could be used to test and also be used as a reference...

--- hardware interfacing/main.py
+++ hardware interfacing/main.py
@@ -1,10 +1,19 @@
 import pyfirmata
 import time
 
-board = pyfirmata.Arduino('/dev/ttyACM0')
+board = pyfirmata.Arduino('COM5')
 
 while True:
     board.digital[13].write(1)
     time.sleep(1)
     board.digital[13].write(0)
     time.sleep(1)
+
+
+
+"""
+
+
+
+
+"""
