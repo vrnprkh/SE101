166e9ac85517394330c8b0ea512bbdcf3e51ee74
em-w <emilywang4321@gmail.com>
2022-11-07 23:29:37 -0500

    functioning magnet detection and leds

--- hardware interfacing/main.py
+++ hardware interfacing/main.py
@@ -1,10 +1,19 @@
 from pyfirmata import Arduino, util
+import time
 
-board = Arduino('COM5')
+board = Arduino('COM3')
+
 iterator = util.Iterator(board)
+iterator.start()
 
-iterator.start
-
-board.analog[10].read()
+testInput = board.get_pin('a:0:i')
 while(True):
-    board
+    i = testInput.read()
+    board.digital[13].write(0)
+    board.digital[12].write(0)
+    if i:
+        if i > 0.7:
+            board.digital[13].write(1)
+        if i < 0.3:
+            board.digital[12].write(1)
+     
