455ac1dfd6d6de75e124006458217c90900bd2d7
em-w <emilywang4321@gmail.com>
2022-11-08 21:59:44 -0500

    added sesnor processing functions

--- hardware interfacing/main.py
+++ hardware interfacing/main.py
@@ -5,14 +5,20 @@ board = Arduino('COM3')
 iterator = util.Iterator(board)
 iterator.start()
 
-testInput = board.get_pin('a:0:i')
+input1 = board.get_pin('a:0:i')
+input2 = board.get_pin('a:1:i')
+input3 = board.get_pin('a:2:i')
+input4 = board.get_pin('a:3:i')
+
+'''
 while(True):
     i = testInput.read()
+    print(i)
     board.digital[13].write(0)
-    board.digital[12].write(0)
+    #board.digital[12].write(0)
     if i:
         if i > 0.7:
             board.digital[13].write(1)
-        if i < 0.3:
-            board.digital[12].write(1)
-     
+        #if i < 0.3:
+            #board.digital[12].write(1)
+'''
