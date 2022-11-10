98832c672b5f6d51cf50a8dc0eacdcde936635a1
em-w <emilywang4321@gmail.com>
2022-11-09 20:27:35 -0500

    test

--- hardware interfacing/main.py
+++ hardware interfacing/main.py
@@ -10,9 +10,9 @@ input2 = board.get_pin('a:1:i')
 input3 = board.get_pin('a:2:i')
 input4 = board.get_pin('a:3:i')
 
-'''
+
 while(True):
-    i = testInput.read()
+    i = input1.read()
     print(i)
     board.digital[13].write(0)
     #board.digital[12].write(0)
@@ -21,4 +21,4 @@ while(True):
             board.digital[13].write(1)
         #if i < 0.3:
             #board.digital[12].write(1)
-'''
+
