1ea8154e97b3a50691a5c15f5baabb6c4e008a33
nandanjp <nandan.jp17@gmail.com>
2022-12-01 16:26:11 -0500

    heck2

--- src/gui/PieceLegalMoves/Rook.py
+++ src/gui/PieceLegalMoves/Rook.py
@@ -27,23 +27,23 @@ def rookLegal(row, col, boardState):
     c = col
     while c < Constants.MAX:
         c += 1
-        if boardState[r][col] == Constants.enemy:
-            moves.append((r, col))
+        if boardState[row][c] == Constants.enemy:
+            moves.append((row, c))
             break
-        if boardState[r][col] != 0:
+        if boardState[row][c] != 0:
             break
 
-        moves.append((r, col))
+        moves.append((row, c))
     
     c = col
     while c > Constants.MIN:
         c -= 1
-        if boardState[r][col] == Constants.enemy:
-            moves.append((r, col))
+        if boardState[row][c] == Constants.enemy:
+            moves.append((row, c))
             break
-        if boardState[r][col] != 0:
+        if boardState[row][c] != 0:
             break
 
-        moves.append((r, col))
+        moves.append((row, c))
     
     return moves
